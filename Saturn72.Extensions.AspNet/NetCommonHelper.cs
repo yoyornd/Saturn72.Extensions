08d5dd350477aaa152fed5ffaedae575fcf6791a
saturn72 <info@saturn72.com>
2015-11-21 00:41:26 +0200

    Remove Methods as they do not represent generic/common functional flow

--- Saturn72.Extensions.AspNet/NetCommonHelper.cs
+++ Saturn72.Extensions.AspNet/NetCommonHelper.cs
@@ -7,57 +7,45 @@ namespace Saturn72.Extensions
 {
     public class NetCommonHelper
     {
-       
+        /// <summary>
+        ///     Finds the trust level of the running application
+        ///     (http://blogs.msdn.com/dmitryr/archive/2007/01/23/finding-out-the-current-trust-level-in-asp-net.aspx)
+        /// </summary>
+        /// <returns>The current trust level.</returns>
+        public static AspNetHostingPermissionLevel GetTrustLevel()
+        {
+            //set minimum
+            var trustLevel = AspNetHostingPermissionLevel.None;
+            foreach (AspNetHostingPermissionLevel level in Enum.GetValues(typeof (AspNetHostingPermissionLevel)))
+            {
+                try
+                {
+                    new AspNetHostingPermission(level).Demand();
+                    trustLevel = level;
+                    break; //we've set the highest permission we can
+                }
+                catch (SecurityException)
+                {
+                }
+            }
+            return trustLevel;
+        }
 
         /// <summary>
-        /// Verifies that a string is in valid e-mail format
+        ///     Verifies that a string is in valid e-mail format
         /// </summary>
         /// <param name="email">Email to verify</param>
         /// <returns>true if the string is a valid e-mail address and false if it's not</returns>
         public static bool IsValidEmail(string email)
         {
-            if (String.IsNullOrEmpty(email))
+            if (string.IsNullOrEmpty(email))
                 return false;
 
             email = email.Trim();
-            var result = Regex.IsMatch(email, "^(?:[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+\\.)*[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!\\.)){0,61}[a-zA-Z0-9]?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\\[(?:(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\.){3}(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\]))$", RegexOptions.IgnoreCase);
+            var result = Regex.IsMatch(email,
+                "^(?:[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+\\.)*[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!\\.)){0,61}[a-zA-Z0-9]?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\\[(?:(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\.){3}(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\]))$",
+                RegexOptions.IgnoreCase);
             return result;
         }
-
-        private static AspNetHostingPermissionLevel? _trustLevel;
-        /// <summary>
-        /// Finds the trust level of the running application (http://blogs.msdn.com/dmitryr/archive/2007/01/23/finding-out-the-current-trust-level-in-asp-net.aspx)
-        /// </summary>
-        /// <returns>The current trust level.</returns>
-        public static AspNetHostingPermissionLevel GetTrustLevel()
-        {
-            if (!_trustLevel.HasValue)
-            {
-                //set minimum
-                _trustLevel = AspNetHostingPermissionLevel.None;
-
-                //determine maximum
-                foreach (AspNetHostingPermissionLevel trustLevel in new[] {
-                                AspNetHostingPermissionLevel.Unrestricted,
-                                AspNetHostingPermissionLevel.High,
-                                AspNetHostingPermissionLevel.Medium,
-                                AspNetHostingPermissionLevel.Low,
-                                AspNetHostingPermissionLevel.Minimal 
-                            })
-                {
-                    try
-                    {
-                        new AspNetHostingPermission(trustLevel).Demand();
-                        _trustLevel = trustLevel;
-                        break; //we've set the highest permission we can
-                    }
-                    catch (SecurityException)
-                    {
-                    }
-                }
-            }
-            return _trustLevel.Value;
-        }
-
     }
 }
