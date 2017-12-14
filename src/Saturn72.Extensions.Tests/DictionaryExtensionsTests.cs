0c6af5d24fcb8965780ddb6250a2d43583e708b8
Saturn Technologies <info@saturn72.com>
2017-12-14 17:53:45 +0200

    add Dictionary extension methods

---
+++ src/Saturn72.Extensions.Tests/DictionaryExtensionsTests.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using Shouldly;
+using Xunit;
+
+namespace Saturn72.Extensions.Tests
+{
+    public class DictionaryExtensionsTests
+    {
+        [Fact]
+        public void GetOrDefault_ReturnsDefault()
+        {
+            var source = new Dictionary<string, string>();
+            var actual = source.GetValueOrDefault("TTT");
+            actual.ShouldBe(default(string));
+
+            actual = source.GetValueOrDefault("TTT", "d");
+            actual.ShouldBe("d");
+        }
+
+        [Fact]
+        public void GetOrDefault_ReturnsValue()
+        {
+            var source = new Dictionary<string, string> { { "TTT", "!" } };
+            var actual = source.GetValueOrDefault("TTT");
+            actual.ShouldBe("!");
+
+            actual = source.GetValueOrDefault("TTT", "d");
+            actual.ShouldBe("!");
+        }
+
+        [Fact]
+        public void GetValueOrSet_Func_ReturnsExistValue()
+        {
+            const string value1 = "value1";
+
+            var source = new Dictionary<string, string>
+            {
+                {"key1", value1},
+                {"key2", "value2"},
+            };
+
+            var actual = source.GetValueOrSet("key1", () => "newValue1");
+            actual.ShouldBe(value1);
+        }
+
+        [Fact]
+        public void GetValueOrSet_Func_ReturnsSetValue()
+        {
+            var source = new Dictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+            };
+
+            const string value3 = "value3";
+            var actual = source.GetValueOrSet("key3", () => value3);
+            actual.ShouldBe(value3);
+        }
+
+        [Fact]
+        public void GetValueOrSet_Simple_ReturnsExistValue()
+        {
+            const string value1 = "value1";
+
+            var source = new Dictionary<string, string>
+            {
+                {"key1", value1},
+                {"key2", "value2"},
+            };
+
+            var actual = source.GetValueOrSet("key1", "newValue1");
+            actual.ShouldBe(value1);
+        }
+
+        [Fact]
+        public void GetValueOrSet_Simple_ReturnsSetValue()
+        {
+            var source = new Dictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+            };
+
+            const string value3 = "value3";
+            var actual = source.GetValueOrSet("key3", value3);
+            actual.ShouldBe(value3);
+        }
+
+        [Fact]
+        public void GetOrDefault_ThrowOnNull()
+        {
+            Should.Throw<NullReferenceException>(
+                () => ((Dictionary<object, object>)null).GetValueOrDefault(new object()));
+        }
+
+
+        [Fact]
+        public void GetOrDefault_ReferenceType_ReturndsDefaultOnNonExistsKey()
+        {
+            var refKey1 = new TestKeyObject { Key = "key" };
+            var refValue1 = new TestValueObject { Value = "key" };
+            var valueTypeSource = new Dictionary<TestKeyObject, TestValueObject>
+            {
+                { refKey1, refValue1}
+            };
+
+            valueTypeSource.GetValueOrDefault(new TestKeyObject()).ShouldBe(default(TestValueObject));
+        }
+
+        [Fact]
+        public void GetOrDefault_ReferenceType_ReturnsValueOnExistsKey()
+        {
+            var refKey1 = new TestKeyObject { Key = "key" };
+            var refValue1 = new TestValueObject { Value = "key" };
+            var valueTypeSource = new Dictionary<TestKeyObject, TestValueObject>
+            {
+                { refKey1, refValue1}
+            };
+
+            valueTypeSource.GetValueOrDefault(refKey1).ShouldBe(refValue1);
+        }
+
+        public class TestKeyObject
+        {
+            public string Key { get; set; }
+        }
+        public class TestValueObject
+        {
+            public string Value { get; internal set; }
+        }
+
+        [Fact]
+        public void GetOrDefault_ValueType_ReturndsDefaultOnNonExistsKey()
+        {
+            var valueTypeSource = new Dictionary<string, int>
+            {
+                { "1", 1 },
+                {"2", 2 }
+            };
+            valueTypeSource.GetValueOrDefault("key").ShouldBe(0);
+        }
+
+        [Fact]
+        public void GetOrDefault_ValueType_ReturnsValueOnExistsKey()
+        {
+            var valueTypeSource = new Dictionary<string, int>
+            {
+                { "1", 1 },
+                {"2", 2 }
+            };
+            valueTypeSource.GetValueOrDefault("2").ShouldBe(2);
+        }
+    }
+}
---
+++ src/Saturn72.Extensions/DictionaryExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Saturn72.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> source, TKey key)
+        {
+            return GetValueOrDefault(source, key, default(TValue));
+        }
+
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> source, TKey key,
+            TValue defaultValue)
+        {
+            TValue tmp;
+            return source.TryGetValue(key, out tmp)
+                ? tmp
+                : defaultValue;
+        }
+        public static TValue GetValueOrSet<TKey, TValue>(this IDictionary<TKey, TValue> source, TKey key,
+            Func<TValue> expression)
+        {
+            TValue tmp;
+            if (source.TryGetValue(key, out tmp))
+                return tmp;
+            return (source[key] = expression());
+        }
+        public static TValue GetValueOrSet<TKey, TValue>(this IDictionary<TKey, TValue> source, TKey key,
+            TValue setValue)
+        {
+            return GetValueOrSet(source, key, () => setValue);
+        }
+    }
+}
