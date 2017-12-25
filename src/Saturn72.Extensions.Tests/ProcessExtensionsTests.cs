f269db8416293776685b12afaed9bd23a32fa8b9
Saturn Technologies <info@saturn72.com>
2017-12-25 17:25:36 +0200

    Add ProcessExtensions

---
+++ src/Saturn72.Extensions.Tests/ProcessExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿#region
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Shouldly;
+using Xunit;
+
+#endregion
+
+namespace Saturn72.Extensions.Tests
+{
+    public class ProcessExtensionsTests
+    {
+       [Fact]
+        public void IsRunning_ThrowsExceptionOnNullProcess()
+        {
+            Should.Throw<NullReferenceException>(() => ((Process)null).IsRunning());
+
+        }
+
+       [Fact]
+        public void IsRunning_ReturnsTrue()
+        {
+            var proc = Process.GetProcesses()[0];
+            proc.IsRunning().ShouldBeTrue();
+        }
+
+        [Fact]
+        public void IsRunning_ReturnsFalseOnNotRunningProcess()
+        {
+            new Process().IsRunning().ShouldBeFalse();
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            string path = Uri.UnescapeDataString(new UriBuilder(codeBase).Path);
+            var exe = Path.Combine(Path.GetDirectoryName(path), "Resources\\SimpleApp.exe");
+
+            var proc = Process.Start(exe);
+            Thread.Sleep(2000);
+            proc.CloseMainWindow();
+            Thread.Sleep(1000);
+            proc.IsRunning().ShouldBeFalse();
+        }
+    }
+}
---
+++ src/Saturn72.Extensions/ProcessExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Saturn72.Extensions
+{
+    public static class ProcessExtensions
+    {
+        public static bool IsRunning(this Process process)
+        {
+            try
+            {
+                return Process.GetProcessById(process.Id).NotNull();
+            }
+            catch (InvalidOperationException)
+            {
+            }
+            catch (ArgumentException)
+            {
+            }
+            return false;
+        }
+    }
+}
