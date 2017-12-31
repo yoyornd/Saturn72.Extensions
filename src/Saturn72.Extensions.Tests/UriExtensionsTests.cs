9c3f944198e578797ac07ef1c780df047ca82ac8
Saturn Technologies <info@saturn72.com>
2017-12-31 20:12:16 +0200

    add UriExtensions

---
+++ src/Saturn72.Extensions.Tests/UriExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿
+using Saturn72.Extensions;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace Saturn72.Extensions.Tests
+{
+    public class UriExtensionsTests
+    {
+        [Theory]
+        [InlineData("relative/url")]
+        [InlineData("/relative/url")]
+        public void UriExtensions_Combine(string relativeUri)
+        {
+            var sourceUri = "http://www.saturn72.com";
+            var uri = new Uri(sourceUri);
+            var cUri = uri.Combine(relativeUri);
+            var expUri = (sourceUri + "/" + relativeUri).Replace("com//", "com/");
+            cUri.ToString().ShouldBe(expUri);
+        }
+    }
+}
---
+++ src/Saturn72.Extensions/UriExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Saturn72.Extensions
+{
+    public static class UriExtensions
+    {
+        public static Uri Combine(this Uri baseUri, string relativePath)
+        {
+            if (relativePath.StartsWith("/"))
+                relativePath = relativePath.Substring(1);
+            var uriBuilder = new UriBuilder(baseUri);
+            uriBuilder.Path += relativePath;
+
+            return uriBuilder.Uri;
+        }
+    }
+}
