74fc7af1e6ce16cf5d47ede61837b2e70a17ec7b
Saturn Technologies <info@saturn72.com>
2018-01-12 13:26:00 +0200

    update mosulr + unit tests

--- src/Saturn72.Extensions/UriExtensions.cs
+++ src/Saturn72.Extensions/UriExtensions.cs
@@ -6,10 +6,10 @@ namespace Saturn72.Extensions
     {
         public static Uri Combine(this Uri baseUri, string relativePath)
         {
-            if (relativePath.StartsWith("/"))
+            while (relativePath.StartsWith("/"))
                 relativePath = relativePath.Substring(1);
             var uriBuilder = new UriBuilder(baseUri);
-            uriBuilder.Path += relativePath;
+            uriBuilder.Path += uriBuilder.Path.EndsWith("/") ? relativePath : "/" + relativePath;
 
             return uriBuilder.Uri;
         }
