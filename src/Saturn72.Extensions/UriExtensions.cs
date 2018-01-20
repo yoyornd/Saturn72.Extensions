618d57d1cf22055a2c33993222842afe4883cc1d
Saturn Technologies <info@saturn72.com>
2018-01-20 02:07:09 +0200

    Add URI Combine and CombineAsQuery

--- src/Saturn72.Extensions/UriExtensions.cs
+++ src/Saturn72.Extensions/UriExtensions.cs
@@ -6,10 +6,24 @@ namespace Saturn72.Extensions
     {
         public static Uri Combine(this Uri baseUri, string relativePath)
         {
-            if (relativePath.StartsWith("/"))
+            while(relativePath.StartsWith("/"))
                 relativePath = relativePath.Substring(1);
+
             var uriBuilder = new UriBuilder(baseUri);
+            uriBuilder.Path += "/";
+            while (uriBuilder.Path.EndsWith("//"))
+                uriBuilder.Path = uriBuilder.Path.Substring(0, uriBuilder.Path.Length - 1);
+
             uriBuilder.Path += relativePath;
+            return uriBuilder.Uri;
+        }
+
+        public static Uri CombineAsQuery(this Uri baseUri, string queryParams)
+        {
+            if (queryParams.StartsWith("/"))
+                queryParams = queryParams.Substring(1);
+            var uriBuilder = new UriBuilder(baseUri);
+            uriBuilder.Path += queryParams;
 
             return uriBuilder.Uri;
         }
